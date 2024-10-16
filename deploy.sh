d12d719b6a7a2ff85a0e4d92a8947335f80c5567
Mark Ingman <mark.ingman@valhalla.software>
2024-10-16 09:48:54 +0100

    Refactor scripts

---
+++ deploy.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+# Deploy actions for deploying as a VM to Google Cloud
+
+source .env
+
+for VAR in NAME PROJECT HOST_ZONE HOST_REGION REPO SERVICE_ACCOUNT
+	do [ -z "${!VAR}" ] && echo "\nERROR: $VAR not set (expected to be in .env file)\n" && exit 1
+done
+
+# Function definitions
+
+deploy() {
+    echo "Build image for Google Cloud Artifact Registry $PROJECT / $REPO / $NAME..."
+
+	TAG=$HOST_REGION-docker.pkg.dev/$PROJECT/$REPO/$NAME:latest
+
+	docker build -f ../Dockerfile --platform linux/amd64 -t $TAG ../
+	docker push $TAG
+}
+
+create() {
+    echo "Create VM $PROJECT / $HOST_ZONE / $NAME..."
+
+	gcloud compute instances create-with-container $NAME \
+		--project=$PROJECT \
+		--zone=$HOST_ZONE \
+		--machine-type=f1-micro \
+		--network-interface=network-tier=PREMIUM,subnet=default,no-address \
+		--maintenance-policy=MIGRATE \
+		--provisioning-model=STANDARD \
+		--service-account=$SERVICE_ACCOUNT \
+		--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+		--image=projects/cos-cloud/global/images/cos-stable-117-18613-0-76 \
+		--boot-disk-size=10GB \
+		--boot-disk-type=pd-balanced \
+		--boot-disk-device-name=$NAME \
+		--container-image=$HOST_REGION-docker.pkg.dev/$PROJECT/$REPO/$NAME:latest \
+		--container-restart-policy=always \
+		--container-command=/app/app \
+		--container-mount-host-path=host-path=/var/data,mode=ro,mount-path=/app/data \
+		--no-shielded-secure-boot \
+		--shielded-vtpm \
+		--shielded-integrity-monitoring \
+		--labels=goog-ec-src=vm_add-gcloud,container-vm=cos-stable-117-18613-0-76 \
+		--reservation-affinity=any \
+		--no-address
+}
+
+reset() {
+    echo "Resetting VM $PROJECT / $HOST_ZONE / $NAME..."
+
+	gcloud compute instances reset $NAME --project=$PROJECT --zone=$HOST_ZONE
+}
+
+update() {
+    echo "Updating data on VM $PROJECT / $HOST_ZONE / $NAME..."
+
+	FILE="disk/conf.json"
+
+	if [ -z "$FILE" ]
+		then
+		echo "\nERROR: could not find $FILE\n" && exit 1
+	fi
+
+	BASE=`basename $FILE`
+
+	echo "Uploading..."
+
+	gcloud compute scp \
+		--project=$PROJECT \
+		--zone=$HOST_ZONE \
+		--recurse \
+		--tunnel-through-iap \
+		--compress \
+		$FILE $NAME:~/
+
+	echo "Copying to position..."
+
+	CMD="sudo chown root:root ~/$BASE; sudo mv ~/$BASE /var/data/$BASE"
+
+	gcloud compute ssh $NAME --project=$PROJECT --zone=$HOST_ZONE --tunnel-through-iap --command "$CMD"
+}
+
+ssh() {
+    echo "SSH to VM $PROJECT / $HOST_ZONE / $NAME..."
+
+	gcloud compute ssh $NAME --project=$PROJECT --zone=$HOST_ZONE
+}
+
+
+# Check if a command was provided
+if [ -z "$1" ]; then
+    echo "Error: No command provided. Available commands: deploy, reset, update, ssh"
+    exit 1
+fi
+
+# Execute the appropriate function based on the command
+case "$1" in
+    deploy)
+        deploy
+        ;;
+    create)
+        create
+        ;;
+    reset)
+        reset
+        ;;
+    update)
+        update
+        ;;
+    ssh)
+        ssh
+        ;;
+    *)
+        echo "Error: Invalid command. Available commands: deploy, create, reset, update, ssh"
+        exit 1
+        ;;
+esac
--- deploy/ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-source ../.env
-
-for VAR in NAME PROJECT HOST_ZONE
-	do [ -z "${!VAR}" ] && echo "\nERROR: $VAR not set\n" && exit 1
-done
-
-gcloud compute ssh $NAME --project=$PROJECT --zone=$HOST_ZONE
